17d8cf541a7508bf3ecde586a579d7200602d950
ziruxi <shao_li_yuan@163.com>
2019-04-23 21:48:30 +0800

    f

--- geekoonline/server/index.js
+++ geekoonline/server/index.js
@@ -4,7 +4,7 @@ require('@babel/register')({
   ignore: [/(node_modules)/],
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
-    '@babel/plugin-syntax-dynamic-import',
+    // '@babel/plugin-syntax-dynamic-import',
     'dynamic-import-node',
     'react-loadable/babel'
   ]
