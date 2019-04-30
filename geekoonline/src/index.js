8cfdc328a3a6ab9dbb66fd09de553200d04c5218
ziruxi <shao_li_yuan@163.com>
2019-04-30 14:56:18 +0800

    ff

--- geekoonline/src/index.js
+++ geekoonline/src/index.js
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './SrollToTop'
 
 ReactDOM.hydrate(<BrowserRouter>
-  <App/>
+  <ScrollToTop>
+    <App/>
+  </ScrollToTop>
 </BrowserRouter>, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
